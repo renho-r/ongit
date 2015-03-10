69d2315c20bbeb6f318a3f478489f3441e37ab57
renho <rho_i_xx@163.com>
2015-03-10 10:51:25 +0800

    20150310_renho_commit_j2setest

---
+++ j2setest/src/j2setest/hash/Test.java
@@ -0,0 +1,15 @@
+package j2setest.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//Map<String, String> map = new HashMap<String, String>();
+//		map.put("renho", "renho");
+//		System.out.println(2 >>> 1);
+//		System.out.println(1 << 2);
+		
+	}
+}
