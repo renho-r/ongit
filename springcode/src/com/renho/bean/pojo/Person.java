4e648bd7df77739a0fcf66f1a674ebecb1863386
renho <rho_i_xx@163.com>
2015-03-24 15:56:41 +0800

    20150324_renho_spring

--- springcode/src/com/renho/bean/pojo/Person.java
+++ springcode/src/com/renho/bean/pojo/Person.java
@@ -2,8 +2,35 @@ package com.renho.bean.pojo;
 
 public class Person {
 
+	private String name;
+	private String password;
+	
 	public Person() {
 		System.out.println("Person init");
 	}
 	
+	public Person(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+	
+	public String getPersonName() {
+		return "personName:renho";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
