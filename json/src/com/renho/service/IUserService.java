315e159424f081fefb5825b3fe3660ebcfb52ab3
renho <rho_i_xx@163.com>
2015-04-23 22:35:21 +0800

    20150423_renho_jsonException

--- json/src/com/renho/service/IUserService.java
+++ json/src/com/renho/service/IUserService.java
@@ -1,9 +1,12 @@
 package com.renho.service;
 
 import com.renho.model.pojo.impl.User;
+import com.renho.util.AppException;
 
 public interface IUserService {
 
 	public int save(User user);
-	
+	public int saveException(User user) throws Exception;
+	public int saveAppException(User user) throws Exception;
+	public int saveRuntimeException(User user);
 }
