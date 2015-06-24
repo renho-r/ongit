a9769fed74c52fc831c992aa4bce1e8ee3aae782
renho <rho_i_xx@163.com>
2015-06-24 17:30:36 +0800

    20150624_renho_commit_flex

--- php/testJs/js/testJs.js
+++ php/testJs/js/testJs.js
@@ -1,3 +1,18 @@
-/**
- * 
- */
+$(function() {
+	$("#testLocation").click(function() {
+		window.location.href = "#name";
+	})
+	$("#changeCss").click(changeRed);
+	
+	function changeBlue() {
+		$("#canChangeCss").attr("href", "/testJs/css/styleblue.css");
+		$("#changeCss").unbind("click");
+		$("#changeCss").click(changeRed);
+	}
+	
+	function changeRed() {
+		$("#canChangeCss").attr("href", "/testJs/css/stylered.css");
+		$("#changeCss").unbind("click");
+		$("#changeCss").click(changeBlue);
+	}
+})
