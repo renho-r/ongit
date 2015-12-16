740abcde332e73062788754c50a99a1fac5e046c
renho <rho_i_xx@163.com>
2015-12-16 16:06:06 +0800

    20151215_renho_commit

--- demo4test/src/main/java/com/jialin/user/action/LoginAction.java
+++ demo4test/src/main/java/com/jialin/user/action/LoginAction.java
@@ -15,75 +15,65 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport {
 
-    private final Log logger = LogFactory.getLog(getClass());
+	private final Log logger = LogFactory.getLog(getClass());
+	private UserEntity userEntity;
 
+	@Resource
+	private IUserService userService;
 
+	@Override
+	public String execute() throws Exception {
 
+		logger.info("项目的根路径：" + PathUtil.root());
+		// logger.error("这是错误信息："+PathUtil.root());
+		HttpServletRequest request = ServletActionContext.getRequest();
+		Map session = ActionContext.getContext().getSession();
+		System.out.println(request.getCharacterEncoding());
 
-    private UserEntity userEntity;
+		if ("".equals(userEntity.getName()) || "".equals(userEntity.getPassword())) {
+			request.setAttribute("loginResult", "请输入用户名和密码！");
+			return "fail";
+		}
 
-    @Resource
-    private IUserService userService;
+		if ("root".equals(userEntity.getName()) && "root123".equals(userEntity.getPassword())) {
+			session.put("userEntity.name", userEntity.getName());
+			System.out.println("登陆成功,用户名为=" + userEntity.getName());
+			logger.info("登陆成功！！！");
+			return "success";
+		}
 
-    @Override
-    public String execute() throws Exception {
+		if (userService.getUser(userEntity) != null) {
+			session.put("userEntity.name", userEntity.getName());
+			System.out.println("登陆成功,用户名为=" + userEntity.getName());
+			logger.info("登陆成功！！！");
+			return "success";
+		}
 
-        logger.info("项目的根路径："+PathUtil.root());
-        //logger.error("这是错误信息："+PathUtil.root());
-        HttpServletRequest request = ServletActionContext.getRequest();
-        Map session = ActionContext.getContext().getSession();
-        System.out.println(request.getCharacterEncoding());
+		logger.info("登陆失败！！！");
+		request.setAttribute("loginResult", "用户名或密码错误！");
+		return "fail";
+	}
 
-        if("".equals(userEntity.getName()) || "".equals(userEntity.getPassword()))
-        {
-            request.setAttribute("loginResult","请输入用户名和密码！");
-            return "fail";
-        }
+	public String loginOut() {
+		Map session = ActionContext.getContext().getSession();
+		session.clear();
+		return "laginOut";
+	}
 
-        if ("root".equals(userEntity.getName()) && "root123".equals(userEntity.getPassword()))
-        {
-            session.put("userEntity.name", userEntity.getName());
-            System.out.println("登陆成功,用户名为=" + userEntity.getName());
-            logger.info("登陆成功！！！");
-            return "success";
-        }
+	public UserEntity getUserEntity() {
+		return userEntity;
+	}
 
+	public void setUserEntity(UserEntity userEntity) {
+		this.userEntity = userEntity;
+	}
 
-        if(userService.getUser(userEntity)!=null)
-        {
-            session.put("userEntity.name", userEntity.getName());
-            System.out.println("登陆成功,用户名为=" + userEntity.getName());
-            logger.info("登陆成功！！！");
-            return "success";
-        }
+	public void setUserService(IUserService userManage) {
+		this.userService = userManage;
+	}
 
-        logger.info("登陆失败！！！");
-        request.setAttribute("loginResult","用户名或密码错误！");
-        return "fail";
-    }
-
-    public String loginOut()
-    {
-        Map session = ActionContext.getContext().getSession();
-        session.clear();
-        return "laginOut";
-    }
-
-
-    public UserEntity getUserEntity() {
-        return userEntity;
-    }
-
-    public void setUserEntity(UserEntity userEntity) {
-        this.userEntity = userEntity;
-    }
-
-    public void setUserService(IUserService userManage) {
-        this.userService = userManage;
-    }
-
-    public IUserService getUserService() {
-        return userService;
-    }
+	public IUserService getUserService() {
+		return userService;
+	}
 
 }
--- demo4test/src/main/java/com/jialin/user/service/impl/UserServiceImpl.java
+++ demo4test/src/main/java/com/jialin/user/service/impl/UserServiceImpl.java
@@ -11,42 +11,42 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.LinkedHashMap;
 
-public class UserServiceImpl extends BasicService<UserEntity,String> implements IUserService {
+public class UserServiceImpl extends BasicService<UserEntity, String> implements IUserService {
 
-    private IUserDao userDao;
+	private IUserDao userDao;
 
-    @Override
-    @Autowired
-    @Qualifier("userDao")    //搜索Qualifier，参考百度百科，就明白这里的意思了
-    public void setBaseDao(IBasicDao<UserEntity, String> userDao) {
-        this.baseDao = userDao;
-        this.userDao = (IUserDao) userDao;
-    }
+	@Override
+	@Autowired
+	@Qualifier("userDao") // 搜索Qualifier，参考百度百科，就明白这里的意思了
+	public void setBaseDao(IBasicDao<UserEntity, String> userDao) {
+		this.baseDao = userDao;
+		this.userDao = (IUserDao) userDao;
+	}
 
-    @Override
-    public UserEntity getUser(UserEntity userEntity) {
-        LinkedHashMap<Object, Object> equalFields =new  LinkedHashMap<Object, Object>();
-        equalFields.put("name", userEntity.getName());
-        equalFields.put("password", userEntity.getPassword());
-        return userDao.get(equalFields,null,null,null,null);
-    }
+	@Override
+	public UserEntity getUser(UserEntity userEntity) {
+		LinkedHashMap<Object, Object> equalFields = new LinkedHashMap<Object, Object>();
+		equalFields.put("name", userEntity.getName());
+		equalFields.put("password", userEntity.getPassword());
+		return userDao.get(equalFields, null, null, null, null);
+	}
 
-    @Override
-    public PageModel<UserEntity> queryUsers(UserEntity userEntity,PageModel<UserEntity> pageModel) {
-        // 初始化Like查询条件承载体
-        LinkedHashMap<String, String> likeFields = null;
+	@Override
+	public PageModel<UserEntity> queryUsers(UserEntity userEntity, PageModel<UserEntity> pageModel) {
+		// 初始化Like查询条件承载体
+		LinkedHashMap<String, String> likeFields = null;
 
-        // 初始化Like查询条件
-        if (userEntity != null) {
-            if (!"".equals(userEntity.getName()) && userEntity.getName() != null) {
-                // 用户名称
-                likeFields = new LinkedHashMap<String, String>();
-                likeFields.put("name", userEntity.getName());
-            }
-        }
+		// 初始化Like查询条件
+		if (userEntity != null) {
+			if (!"".equals(userEntity.getName()) && userEntity.getName() != null) {
+				// 用户名称
+				likeFields = new LinkedHashMap<String, String>();
+				likeFields.put("name", userEntity.getName());
+			}
+		}
 
-        LinkedHashMap<String, String> orderByFields =new  LinkedHashMap<String, String>();
-        orderByFields.put("name", "DESC");
-        return userDao.queryPageModelByLike(likeFields,pageModel,orderByFields);
-    }
+		LinkedHashMap<String, String> orderByFields = new LinkedHashMap<String, String>();
+		orderByFields.put("name", "DESC");
+		return userDao.queryPageModelByLike(likeFields, pageModel, orderByFields);
+	}
 }
