6884a0f30ed8c5ff7888f0ad8a5963aed9d3fc4d
renho <renho@192.168.1.103>
2017-02-27 22:26:11 +0800

    20170227_renho_commit_jersey-https

--- jerseyrestful/src/main/java/com/renho/jersey/appconfig/MyApplication.java
+++ jerseyrestful/src/main/java/com/renho/jersey/appconfig/MyApplication.java
@@ -2,11 +2,16 @@ package com.renho.jersey.appconfig;
 
 import javax.ws.rs.ApplicationPath;
 
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.server.ResourceConfig;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 @ApplicationPath("resources")
 public class MyApplication extends ResourceConfig {
     public MyApplication() {
         packages("com.renho.jersey;com.renho.jersey.filter;com.renho.jersey.namebinding;com.renho.jersey.dynamicbinding");
+        register(LoggingFilter.class);
+        register(JacksonJsonProvider.class);
     }
 }
