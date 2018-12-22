69c182614e41b8dbfbdfac89ba26de61c73aa0e9
renho <renho@thinkpad.com>
2018-12-22 13:12:31 +0800

    20181222_renho_commit_springboot-code

---
+++ springboot-code/src/test/java/com/renho/springbootcode/SpringbootCodeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.renho.springbootcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootCodeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
