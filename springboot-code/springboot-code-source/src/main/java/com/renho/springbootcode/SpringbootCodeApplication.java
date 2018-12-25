8302424d17ea40543ac2e5ac772666f93f3eb9c7
renho <rho_i_xx@163.com>
2018-12-26 00:38:19 +0800

    20181222_renho_commit_springboot-code

--- springboot-code/springboot-code-source/src/main/java/com/renho/springbootcode/SpringbootCodeApplication.java
+++ springboot-code/springboot-code-source/src/main/java/com/renho/springbootcode/SpringbootCodeApplication.java
@@ -1,15 +1,28 @@
 package com.renho.springbootcode;
 
+import com.renho.springbootcode.domain.pojo.TbUser;
+import com.renho.springbootcode.mapper.TbUserMapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.List;
+
+/**
+ * @author renho
+ */
 @SpringBootApplication
+@MapperScan("com.renho.springbootcode.mapper")
 public class SpringbootCodeApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SpringbootCodeApplication.class, args);
-        System.out.println(context.getBean("myStarterService"));
+        TbUserMapper userMapper = (TbUserMapper) context.getBean("tbUserMapper");
+        List<TbUser> users = userMapper.findAllUser();
+        users.forEach(System.out::println);
+
+        userMapper.insert(new TbUser());
     }
 
 }
