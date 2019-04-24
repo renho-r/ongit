1e7f34508312fd9a4c0c092ec8d61fbef485a32b
renho <renho@thinkpad.com>
2019-04-24 17:27:26 +0800

    20190424_renho_commit_spring-webflux

---
+++ spring-webflux/src/main/java/com/renho/springwebflux/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.renho.springwebflux.controller;
+
+import com.renho.springwebflux.domain.User;
+import com.renho.springwebflux.exception.ResourceNotFoundException;
+import com.renho.springwebflux.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+/**
+ * @author: xxxxx
+ * @since: 1.0.0
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public Mono<String> notFound() {
+        System.out.println("404-renho");
+        return Mono.just("404-renho");
+    }
+
+    @GetMapping("")
+    public Flux<User> list() {
+        return this.userService.list();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<User> getById(@PathVariable("id") final String id) {
+        return this.userService.getById(id);
+    }
+
+    @PostMapping("")
+    public Mono<User> create(@RequestBody final User user) {
+        return this.userService.createOrUpdate(user);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<User>  update(@PathVariable("id") final String id, @RequestBody final User user) {
+        Objects.requireNonNull(user);
+        user.setId(id);
+        return this.userService.createOrUpdate(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<User>  delete(@PathVariable("id") final String id) {
+        return this.userService.delete(id);
+    }
+}
