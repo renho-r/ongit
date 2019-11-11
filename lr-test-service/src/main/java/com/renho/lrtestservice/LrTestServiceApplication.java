971bc7721dd3836f3ae534eb1c588fbd8b7a792f
renho <renho@thinkpad.com>
2019-11-11 17:52:37 +0800

    20191111_renho_commit_test

---
+++ lr-test-service/src/main/java/com/renho/lrtestservice/LrTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.renho.lrtestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrTestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LrTestServiceApplication.class, args);
+    }
+
+}
---
+++ lr-test-service/src/test/java/com/renho/lrtestservice/LrTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renho.lrtestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrTestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
